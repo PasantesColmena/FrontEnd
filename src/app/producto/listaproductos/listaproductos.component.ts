a09133d7a63c54144720799bffde35920bbab167
JeremySO202 <jeremyserracin@gmail.com>
2021-12-02 14:16:52 -0600

    Update listaproductos.component.ts

--- src/app/producto/listaproductos/listaproductos.component.ts
+++ src/app/producto/listaproductos/listaproductos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import swal from 'sweetalert';
+
 
 import { Producto } from '../service/productos/productos';
 import { ProductoService } from '../service/productos/productos.service';
